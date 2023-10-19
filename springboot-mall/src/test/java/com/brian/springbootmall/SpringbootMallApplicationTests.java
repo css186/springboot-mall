d6693b7dbee7aa81b335f785e30fe1b9ad08d98a
css186 <cssrm31@gmail.com>
2023-10-19 10:21:44 +0900

    init spring boot project

---
+++ springboot-mall/src/test/java/com/brian/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
